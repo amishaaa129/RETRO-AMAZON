d125acc17ece5c2b33e15d38c1647c26aa7a6453
unknown <amishawilliams2019@gmail.com>
2024-12-17 02:04:17 +0530

    Did some changes with postgres to deploy on vercel.

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import pg from "pg";
+import dotenv from 'dotenv';
+dotenv.config();
 
+const { Pool } = pg;
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "retro-amazon",
-  password: "strangert3",
-  port: 5432,
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
-db.connect();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -24,12 +26,19 @@ app.get("/", (req,res) =>{
 })
 
 app.post("/", async (req,res) =>{
-  const name=req.body.name;
-  const selectedCategory = req.body.poll; // Get the selected category from the form
+  const { poll: selectedCategory } = req.body;
+    const name = req.body.name; 
 
-    // Insert the selected category into the PostgreSQL database
-    await db.query("INSERT INTO poll_votes (name,category) VALUES ($1,$2)",[name,selectedCategory]);
-    res.redirect("/");
+    try {
+        await pool.query(
+            'INSERT INTO poll_votes (name, category) VALUES ($1, $2)',
+            [name, selectedCategory]
+        );
+        res.redirect('/');
+    } catch (err) {
+        console.error('Error inserting data into the database:', err);
+        res.status(500).send('An error occurred');
+    }
     });
 
 app.listen(port);
